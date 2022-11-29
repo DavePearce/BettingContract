e57a6d5f2986e45248a0c4fdef3f6bbe52af8668
DavePearce <dave01001110@gmail.com>
2022-11-29 14:41:47 +1300

    Update to betting contract

--- src/betting.sol
+++ src/betting.sol
@@ -7,13 +7,15 @@ pragma solidity ^0.8.7;
  * is that there is no benefit to betting the full amount in one go,
  * because you'd just receive your own bet back.  In otherwords, to
  * make any money, you need at least one other player to bet!
- *
- * The contract currently contains bug in that money can end up being
- * locked into the contract forever!
  */
 contract Betting {
-    uint public counter = 1000;
+    uint public counter = 0;
 
+    /**
+     * Make a bet by transfering some ether.  The amount transferred
+     * is deducted from the current target.  When the target is
+     * reached, the balance is paid out.
+     */
     function bet() public payable {
         // Prevent zero bets.
         require(msg.value <= counter);
@@ -25,9 +27,22 @@ contract Betting {
         // Check if winner
         if(counter == 0) {
             // Counter reached zero, so pay out.
-            payable(msg.sender).transfer(address(this).balance);
-            // Reset counter
-            counter = 1000;
-        } 
+            payable(msg.sender).transfer(address(this).balance);            
+        }
+    }
+
+    /**
+     * Restart betting after someone has won.  Anyone can call this
+     * and set the target for the next round.  A limit of one ether is
+     * imposed to prevent someone setting an unreasonably large
+     * amount.
+     */
+    function reset(uint target) public {
+        // Target cannot exceed 1 eth
+        require(target <= 1 ether);
+        // Cannot reset unless betting finished.
+        require(counter == 0);
+        // Perform the reset!
+        counter = target;
     }
 }
