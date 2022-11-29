7019452314a5125cd0b484c665bb4482e10944ed
DavePearce <dave01001110@gmail.com>
2022-11-29 15:24:48 +1300

    Add README.md file

--- src/betting.sol
+++ src/betting.sol
@@ -9,40 +9,45 @@ pragma solidity ^0.8.7;
  * make any money, you need at least one other player to bet!
  */
 contract Betting {
-    uint public counter = 0;
-
-    /**
-     * Make a bet by transfering some ether.  The amount transferred
-     * is deducted from the current target.  When the target is
-     * reached, the balance is paid out.
-     */
-    function bet() public payable {
-        // Prevent zero bets.
-        require(msg.value <= counter);
-        // Apply the best
-        unchecked {
-            // NOTE: cannot underflow as msg.value <= counter.
-            counter = counter - msg.value;
-        }
-        // Check if winner
-        if(counter == 0) {
-            // Counter reached zero, so pay out.
-            payable(msg.sender).transfer(address(this).balance);            
-        }
+  /**
+   * The current betting target.  When this reaches zero, the
+   * available balance is paid out.  The target should never be more
+   * than one ether.
+   */
+  uint public target = 0;
+  
+  /**
+   * Make a bet by transfering some ether.  The amount transferred
+   * is deducted from the current target.  When the target is
+   * reached, the balance is paid out.
+   */
+  function bet() public payable {
+    // Prevent zero bets.
+    require(msg.value <= target);
+    // Apply the best
+    unchecked {
+      // NOTE: cannot underflow as msg.value <= target.
+      target = target - msg.value;
     }
-
-    /**
-     * Restart betting after someone has won.  Anyone can call this
-     * and set the target for the next round.  A limit of one ether is
-     * imposed to prevent someone setting an unreasonably large
-     * amount.
-     */
-    function reset(uint target) public {
-        // Target cannot exceed 1 eth
-        require(target <= 1 ether);
-        // Cannot reset unless betting finished.
-        require(counter == 0);
-        // Perform the reset!
-        counter = target;
+    // Check if winner
+    if(target == 0) {
+      // Target reached zero, so pay out.
+      payable(msg.sender).transfer(address(this).balance);            
     }
+  }
+  
+  /**
+   * Restart betting after someone has won.  Anyone can call this
+   * and set the target for the next round.  A limit of one ether is
+   * imposed to prevent someone setting an unreasonably large
+   * amount.
+   */
+  function reset(uint newTarget) public {
+    // Target cannot exceed 1 eth
+    require(newTarget <= 1 ether);
+    // Cannot reset unless betting finished.
+    require(target == 0);
+    // Perform the reset!
+    target = newTarget;
+  }
 }
